8244a3ba7e69e2de661568e29e2922d8094cf23e
Angelica <hernandez.angelica77@gmail.com>
2014-11-05 15:01:04 -0500

    assign
    week2 assignment of part 1

---
+++ Week2/Assignmetns/Assignment 1/fruit.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Fruit Database Application</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Font -->
+    <link href='http://fonts.googleapis.com/css?family=Kameron' rel='stylesheet' type='text/css'>
+</head>
+<body>
+    <section class="border">
+        <h1>Let's Talk Fruit</h1>
+
+        <div class="img">
+            <img src="images/fruits.jpg">
+        </div>
+
+        <form class="fruitForm" action="fruit.php" method="post">
+            Fruit:<input type="text" name="fruitname" required=""/>
+            Color:<input type="text" name="fruitcolor" required=""/><br />
+            <input class="btn" type="submit" name="submit" value="Submit" />
+        </form>
+    </section>
+</body>
+</html>
